6e1dd4dc79e125c1500d50c8efc595e54429d358
gold2718 <goldy@ucar.edu>
2019-01-22 11:07:36 -0700

    Updated CMakeLists.txt to call ccpp_capgen and start rework of main routing.

--- cam_driver/src/cam_kessler.F90
+++ cam_driver/src/cam_kessler.F90
@@ -1,184 +1,93 @@
 module cam_kessler_main
 
-use machine, only: kind_phys
-
-implicit none
-
-contains
-
-subroutine cam_kessler_main_sub()
-
-  use ppgrid, only: pcols, pver, pverp
-
-  ! Add the CCPP specific types and functions
-  use :: ccpp_api,                           &
-         only: ccpp_t,                       &
-               ccpp_init,                    &
-               ccpp_finalize,                &
-               ccpp_physics_init,            &
-               ccpp_physics_run,             &
-               ccpp_physics_finalize,        &
-               ccpp_field_add
-
-! NOTE -- The variables managed by the CCPP are included in the the ccpp_modules.inc file in the "use" statements
-#include "ccpp_modules.inc"
+  use machine, only: kind_phys
 
   implicit none
 
+contains
 
-  integer                           :: i, j
+  subroutine cam_kessler_main_sub()
 
-  ! Create the CCPP required cdata structure
-  type(ccpp_t), allocatable, target                      :: cdata(:)
+    use ppgrid,        only: pcols, pver, pverp
+    use physics_types, only: state, tend, physics_state
+    use CAM_ccpp_cap,  only: CAM_ccpp_physics_initialize
+    use CAM_ccpp_cap,  only: CAM_ccpp_physics_timestep_initial
+    use CAM_ccpp_cap,  only: CAM_ccpp_physics_run
+    use CAM_ccpp_cap,  only: CAM_ccpp_physics_timestep_final
+    use CAM_ccpp_cap,  only: CAM_ccpp_physics_finalize
 
-  integer                                                :: ierr
-  integer ,parameter :: ncols=1
-  integer ,parameter :: nlevs=8
-  integer ,parameter :: ntimes=11
+    integer, parameter :: begchunk = 33 ! Not needed for CAM7
+    integer, parameter :: endchunk = 33 ! Not needed for CAM7
+    integer, parameter :: ncols = 1
+    integer, parameter :: ntimes = 11
 
-  real(kind_phys) :: rair, cpair, latvap, pstd, rhoh2o, ztodt, dt
-  integer :: icols, iver
+    integer            :: i, j
+    integer            :: ierr
+    integer            :: col_start, col_end
+    integer            :: ncol, nwrite
+    character(len=20)  :: string
+    character(len=512) :: errmsg
+    integer            :: errflg
 
-  real(kind_phys),allocatable     :: rho(:,:)  ! Dry air density
-  real(kind_phys),allocatable     :: pk(:,:)   ! exner func.
-  real(kind_phys),allocatable     :: th(:,:)   ! Potential temp.
-  real(kind_phys),allocatable     :: qv(:,:)   ! Water vapor
-  real(kind_phys),allocatable     :: qc(:,:)   ! Cloud water
-  real(kind_phys),allocatable     :: qr(:,:)   ! Rain water
-  real(kind_phys),allocatable     :: z(:,:)    ! height
-  real(kind_phys),allocatable     :: precl(:)
-  real(kind_phys),allocatable     :: precl_prev(:)
-  real(kind_phys),allocatable     :: qv_prev(:,:)   ! Water vapor
-  real(kind_phys),allocatable     :: qc_prev(:,:)   ! Cloud water
-  real(kind_phys),allocatable     :: qr_prev(:,:)   ! Rain water
-  real(kind_phys),allocatable     :: t_prev(:,:)   ! potential temperature
-  real(kind_phys),allocatable     :: ttend_qc(:,:)
-  real(kind_phys),allocatable     :: ttend_qv(:,:)
-  real(kind_phys),allocatable     :: ttend_qr(:,:)
-  real(kind_phys),allocatable     :: ttend_t(:,:)
+    ! Allocate the host variables
+    call physics_type_alloc(phys_state, phys_tend, begchunk, endchunk, psetcols)
 
-  integer  :: istep, ncol, pver_in, nwrite, nz
-  character(len=20) :: string
-  character(len=512) :: errmsg
-  integer :: errflg
+    !  open(unit=50, file='../src/fort.50')
+    !  read(50, fmt='(2i4)') icols, iver
 
-  ! Allocate the host variables
-  allocate(k_rateConst(3))
-  allocate(my_co(nlevs))
-  allocate(my_o3(nlevs))
-  allocate(cdata(ncols))
+    ! Use the suite information to setup the run
+    call CAM_ccpp_physics_initialize('cam_kessler_test')
 
+    ! loop over all time steps
+    nwrite=0
+    do j = 1, ntimes
+!      read(50,fmt='(a10,i4)') string(1:19),nwrite
+!      read(50,fmt='(a20,2i4,f20.13)') string(1:19),ncol, pver_in, ztodt
+!      read(50,fmt='(a10,(e22.15))') string,rho(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,z(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,pk(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,th(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,qv(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,qc(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,qr(:ncol,:)
+!      read(50,fmt='(a10,(e22.15))') string,precl(:ncol)
+      do i = 1, ncols
+        call CAM_ccpp_physics_run('cam_kessler_test', 'physics')
+      write(6,*) 'At time step', j, 'in host model Temperature =', state%T(1, pver)
+        ! write(51,'(a10,i4)') 'nwrite=',nwrite
+        ! write(51,'(a20,2i4,f20.13)') 'ncol, pver, ztodt=',ncol, pver, ztodt
+        ! write(51,fmt='(a10,(e22.15))') 'rho=',rho(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'z=',z(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'pk=',pk(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'th=',th(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'qv=',qv(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'qc=',qc(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'qr=',qr(:ncol,:)
+        ! write(51,fmt='(a10,(e22.15))') 'precl=',precl(:ncol)
+        nwrite = nwrite + 1
 
-  ! Initialize the host variables
-  state_host%Temperature = 200.
-  my_co(:) = 100_kind_phys
-  my_o3(:) = 1e-6_kind_phys
-
-  open(unit=50, file='../src/fort.50')
-  read(50, fmt='(2i4)') icols, iver
-  read(50, fmt='(5f22.13)') rair, cpair, latvap, pstd, rhoh2o
-
-  allocate(rho(pcols,pver))
-  allocate(z(pcols,pver))
-  allocate(pk(pcols,pver))
-  allocate(th(pcols,pver))
-  allocate(qv(pcols,pver))
-  allocate(qc(pcols,pver))
-  allocate(qr(pcols,pver))
-
-! These shouldn't need to be added at the host model, but I put them into cam_var_defs because couldn't get kessler_var_defs setup properly -- CAC
-  allocate(precl(pcols))
-  allocate(precl_prev(pcols))
-  allocate(qv_prev(pcols,pver))
-  allocate(qc_prev(pcols,pver))
-  allocate(qr_prev(pcols,pver))
-  allocate(t_prev(pcols,pver))
-  allocate(ttend_qc(pcols,pver))
-  allocate(ttend_qr(pcols,pver))
-  allocate(ttend_qv(pcols,pver))
-  allocate(ttend_t(pcols,pver))
-
-  do i = 1, ncols
-
-      ! Use the suite information to setup the run
-      call ccpp_init( '../suites/suite_cam_kessler_test_simple1.xml', cdata(i), ierr)
-      if (ierr/=0) then
-          write(*,'(a,i0,a)') 'An error occurred in ccpp_init for column ', i, '. Exiting...'
+        if (ierr /= 0) then
+          write(*,*) errmsg
+          write(*,'(a,i0,a)') 'An error occurred in CAM_ccpp_physics_run for column ', i, '. Exiting...'
           stop
-      end if
+        end if
+      end do
 
-! use ccpp_fields.inc to call ccpp_field_add for all variables to be exposed to CCPP (this is auto-generated from /src/ccpp/scripts/ccpp_prebuild.py - the script parses tables in the cam_var_defs.f90)
-#include "ccpp_fields.inc"
+      ! Decrease the temperature every time step
+      state_host%Temperature = state_host%Temperature - 100._kind_phys
 
-      ! initialize each column's physics
-      call ccpp_physics_init(cdata(i), ierr=ierr)
-      if (ierr/=0) then
-          write(*,'(a,i0,a)') 'An error occurred in ccpp_physics_init for column ', i, '. Exiting...'
-          stop
-      end if
+    end do
 
-  end do
 
-  write(6,*) ' '
-  write(6,*) 'After initialization, my_co(1)=',my_co(1)
-  write(6,*) 'After initialization, my_o3(1)=',my_o3(1)
-  write(6,*) ' '
-
-  ! loop over all time steps
-  nwrite=0
-  do j = 1, ntimes
-      read(50,fmt='(a10,i4)') string(1:19),nwrite
-      read(50,fmt='(a20,2i4,f20.13)') string(1:19),ncol, pver_in, ztodt
-      read(50,fmt='(a10,(e22.15))') string,rho(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,z(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,pk(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,th(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,qv(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,qc(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,qr(:ncol,:)
-      read(50,fmt='(a10,(e22.15))') string,precl(:ncol)
-    write(6,*) 'At time step', j, 'in host model state_host%Temperature =', state_host%Temperature
     do i = 1, ncols
-     call ccpp_physics_run(cdata(i), ierr=ierr)
-!    call kessler_run(ncol, pver, ztodt, rho(:ncol,:), z(:ncol,:), pk(:ncol,:),    &
-!         th(:ncol,:), qv(:ncol,:), qc(:ncol,:), qr(:ncol,:), precl(:ncol), errmsg)
-      write(51,'(a10,i4)') 'nwrite=',nwrite
-      write(51,'(a20,2i4,f20.13)') 'ncol, pver, ztodt=',ncol, pver, ztodt
-      write(51,fmt='(a10,(e22.15))') 'rho=',rho(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'z=',z(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'pk=',pk(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'th=',th(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'qv=',qv(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'qc=',qc(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'qr=',qr(:ncol,:)
-      write(51,fmt='(a10,(e22.15))') 'precl=',precl(:ncol)
-      nwrite = nwrite+1
-
-       if (ierr/=0) then
-           write(*,*) errmsg
-           write(*,'(a,i0,a)') 'An error occurred in ccpp_physics_run for column ', i, '. Exiting...'
-           stop
-       end if
-
-       write(6,*) ' At time j=',j,' my_co(1)=',my_co(1)
-       write(6,*) ' At time j=',j,' my_o3(1)=',my_o3(1)
-     end do
-
-     ! Decrease the temperature every time step
-     state_host%Temperature = state_host%Temperature - 100._kind_phys
-
-  end do
-
-
-  do i=1, ncols
-      call ccpp_finalize(cdata(i), ierr)
+      call ccpp_timestep_final('cam_kessler_test')
       if (ierr/=0) then
-          write(*,'(a,i0,a)') 'An error occurred in ccpp_finalize for column ', i, '. Exiting...'
-          stop
+        write(*,'(a,i0,a)') 'An error occurred in ccpp_finalize for column ', i, '. Exiting...'
+        stop
       end if
-  end do
+    end do
 
-end subroutine cam_kessler_main_sub
+  end subroutine cam_kessler_main_sub
 
 end module cam_kessler_main
 
--- cam_driver/src/ppgrid.F90
+++ cam_driver/src/ppgrid.F90
@@ -1,7 +1,7 @@
 module ppgrid
 
 integer,parameter :: pcols=16
-integer,parameter :: pver=30
+integer,parameter :: pver=8
 integer,parameter :: pverp=pver+1
 integer,parameter :: pcnst=3
 
